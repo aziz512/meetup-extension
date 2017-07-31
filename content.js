8558817f76ee66b080a0001ea1b3b7250d41d668
Aziz Yokubjonov <yakubjanov0@hotmail.com>
2017-07-31 16:46:36 -0400

    send message to those without full name

--- content.js
+++ content.js
@@ -1,20 +1,57 @@
-window.onload = function () {
-  console.log('aa');
-}
+localStorage['meetup-extension'] = localStorage['meetup-extension'] ? localStorage['meetup-extension'] : JSON.stringify({ people:{} });
 
+window.onload = messageMember;
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     switch(request.type){
       case 'REPLACE_EDITOR':
         replaceEditor();
+        break;
+      case 'MESSAGE_ABOUT_NAME':
+        messageMembers();
     }
   }
 );
+
+function messageMembers() {
+  var membersLinks = [].slice.call(document.getElementsByClassName('member-name')).map(h5 => h5.querySelector('a'));
+  var fullNameRegex = /[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{2,} [a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{2,}/;
+  membersLinks = membersLinks.filter(a => !fullNameRegex.test(a.innerText));
+  var storage = JSON.parse(localStorage['meetup-extension']);
+  membersLinks.forEach(link => {
+    storage.people[link.href] = false;
+    link.target = '_blank';
+    link.click();
+  });
+  storage.lastHref = membersLinks[membersLinks.length-1].href;
+  localStorage['meetup-extension'] = JSON.stringify(storage);
+}
+
 
 function replaceEditor(){
   var script = document.createElement('script');
   script.src = 'https://rawgit.com/aziz512/meetup-extension/master/addScripts.js';
   script.type = 'text/javascript';
   document.head.appendChild(script);
+}
+
+
+function messageMember(){
+  var storage = JSON.parse(localStorage['meetup-extension']);
+  if(window.location.href.includes('members/') && !storage.people[window.location.href]){
+    var msgBtn = document.getElementsByClassName('icon-messaging-messages-outline')[0];
+    if(msgBtn){
+      msgBtn.click();
+      document.getElementById('messaging-new-convo').value = 'Hello! in order to attend our meetups please edit your profile and add your full name.';
+      document.getElementById('messaging-new-send').disabled = false;
+      document.getElementById('messaging-new-send').click();
+    }
+    storage.people[window.location.href] = true;
+    if(window.location.href === storage.lastHref){
+      storage = {people:{}};
+    }
+    localStorage['meetup-extension'] = JSON.stringify(storage);
+
+  }
 }
--- popup.js
+++ popup.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("replaceEditorButton").addEventListener("click", replaceEditor);
+
+  document.getElementById("sendOutReminderButton").addEventListener("click", sendOutReminder);
 });
 
 
@@ -7,5 +9,12 @@ function replaceEditor(){
   chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
     var activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {"type": "REPLACE_EDITOR"});
+  });
+}
+
+function sendOutReminder(){
+  chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {"type": "MESSAGE_ABOUT_NAME"});
   });
 }
